e132079f500c9cd980a41e75f8515d6715a45092
3wasfnjd <44668762+3wasfnjd@users.noreply.github.com>
2026-09-20 02:08:34 +0300

    Point Matching Puzzle code at normalized image assets

--- puzzle-kit/matching/assets.js
+++ puzzle-kit/matching/assets.js
@@ -1,5 +1,12 @@
+const here=import.meta.url;
+const asset=name=>new URL(`./assets/original/${name}`,here).href;
+
 export const matchingAssets=Object.freeze({
-  base:null,
-  solved:null,
-  symbols:Object.freeze([])
+  base:asset('board.png'),
+  solved:asset('solution.png'),
+  cables:Object.freeze({
+    blue:asset('cable-blue.png'),
+    orange:asset('cable-orange.png'),
+    purple:asset('cable-purple.png')
+  })
 });
